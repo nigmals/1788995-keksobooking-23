b23481ff3fee736f2dfb01b84f35a6c5c4497423
alsu <als@kxhfc.com>
2021-06-02 21:09:28 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
-function getRandomInteger (min, max){
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if ( min < 0 || max < 0 || max <= min) {
+function getRandomIntNumFromIntRange (min, max) {
+  const firstNumber = Math.ceil(min);
+  const secondNumber = Math.floor(max);
+  const numbersOfInterval = Math.floor(max) - Math.ceil(min);
+
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    return ('Оба аргумента не должны быть строкой');
+  }
+  if (numbersOfInterval < 0) {
+    return 'Оба аргумента должны быть целыми числами';
+  }
+  if (firstNumber < 0 || secondNumber < 0 || secondNumber <= firstNumber) {
     return 'Оба аргумента должны быть положительными и  второй аргумент больше первого!';
   }
-  return Math.floor(min + Math.random() * (max + 1 - min));
+
+  return Math.floor(firstNumber + Math.random() * (secondNumber + 1 - firstNumber));
 }
 
-getRandomInteger(1,6);
+getRandomIntNumFromIntRange(1.2, 2.2);
 
-function getRandomFromRange (min, max , places) {
-  if ( min < 0 || max < 0 || max <= min) {
-    return 'Оба аргумента должны быть положительными и  второй аргумент больше первого!';
+function getRandomFloatNumFromRange (min, max , places) {
+  if (typeof min !== 'number' || typeof max !== 'number' || typeof places !== 'number') {
+    return ('Все аргументы не должны быть строкой');
   }
-  const RESULT = Math.random() * (max - min) + min;
-  return RESULT.toFixed(places);
+  if (places <= 0){
+    return 'Третий аргумент должен быть больше нуля';
+  }
+  if (min < 0 || max < 0 || max <= min) {
+    return 'Все аргументы должны быть положительными и второй аргумент больше первого!';
+  }
+
+  const number = Math.random() * (max - min) + min;
+  return parseFloat(number.toFixed(places));
 }
 
-getRandomFromRange(0.0, 1.2, 1);
+getRandomFloatNumFromRange(0.1, 0.2, 1);
