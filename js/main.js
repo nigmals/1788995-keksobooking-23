24fca06d8f08f7266a8810796552be5e17e2a7c8
alsu <als@kxhfc.com>
2021-06-03 15:46:22 +0300

    Добавление Error и исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
-function getRandomIntNumFromIntRange (min, max) {
-  const firstNumber = Math.ceil(min);
-  const secondNumber = Math.floor(max);
-  const numbersOfInterval = Math.floor(max) - Math.ceil(min);
+function getRandomIntegerFromRange (min, max) {
+  const ceiledMin = Math.ceil(min);
+  const ceiledMax = Math.floor(max);
+  const intervalLength = Math.floor(max) - Math.ceil(min);
 
   if (typeof min !== 'number' || typeof max !== 'number') {
-    return ('Оба аргумента не должны быть строкой');
+    throw  new Error ('Оба аргумента не должны быть строкой');
   }
-  if (numbersOfInterval < 0) {
-    return 'Оба аргумента должны быть целыми числами';
+  if (intervalLength <= 0) {
+    throw  new Error ('Некорректные границы интервала');
   }
-  if (firstNumber < 0 || secondNumber < 0 || secondNumber <= firstNumber) {
-    return 'Оба аргумента должны быть положительными и  второй аргумент больше первого!';
+  if (ceiledMin < 0 || ceiledMax< 0 || ceiledMax <= ceiledMin) {
+    throw  new Error ('Оба аргумента должны быть положительными и  второй аргумент больше первого!');
   }
 
-  return Math.floor(firstNumber + Math.random() * (secondNumber + 1 - firstNumber));
+  return Math.floor(ceiledMin + Math.random() * (ceiledMax + 1 - ceiledMin));
 }
 
-getRandomIntNumFromIntRange(1.2, 2.2);
+getRandomIntegerFromRange(1.2, 2.2);
 
-function getRandomFloatNumFromRange (min, max , places) {
+function getRandomFloatFromRange (min, max, places) {
+  const intervalLength = max - min;
+
   if (typeof min !== 'number' || typeof max !== 'number' || typeof places !== 'number') {
-    return ('Все аргументы не должны быть строкой');
+    throw  new Error ('Все аргументы не должны быть строкой');
   }
-  if (places <= 0){
-    return 'Третий аргумент должен быть больше нуля';
+  if (intervalLength <= 0) {
+    throw  new Error ('Некорректные границы интервала');
+  }
+  if (places <= 0) {
+    throw  new Error ('Третий аргумент должен быть больше нуля');
   }
   if (min < 0 || max < 0 || max <= min) {
-    return 'Все аргументы должны быть положительными и второй аргумент больше первого!';
+    throw  new Error ('Все аргументы должны быть положительными и второй аргумент больше первого!');
   }
 
   const number = Math.random() * (max - min) + min;
   return parseFloat(number.toFixed(places));
 }
 
-getRandomFloatNumFromRange(0.1, 0.2, 1);
+getRandomFloatFromRange(0.1, 0.2, 1);
