64d920a24a3ce46df0cb17e1534e42bc80814ea6
alsu <als@kxhfc.com>
2021-06-13 21:11:27 +0300

    Добавление функций и исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,76 +1,99 @@
-/* eslint-disable indent */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-undef */
-/* eslint-disable no-template-curly-in-string */
 const TYPES_OF_HOUSING = [
-    'palace',
-    'flat',
-    'house',
-    'bungalow',
-    'hotel',
-  ];
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
 
-  const CHECKIN_TIMES = [
-    '12:00',
-    '13:00',
-    '14:00',
-  ];
+const CHECKIN_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 
-  const CHECKOUT_TIMES = [
-    '12:00',
-    '13:00',
-    '14:00',
-  ];
+const CHECKOUT_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 
-  const ALL_FEATURES = [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-  ];
+const ALL_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+];
 
-  const ALL_PHOTOS = [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ];
+const ALL_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 
-  const SIMILAR_COUNT = 10;
+const SIMILAR_COUNT = 10;
 
-  const createAd = () => ({
-    author: {
-      avatar: 'img/avatars/user0${getRandomPositiveInteger(1, 10)}.png',
-    },
-    offer: {
-      title: 'Заголовок',
-      address: 'Широта: ${getRandomPositiveFloat(35.65000,  35.70000, 5)}, Долгота: ${getRandomPositiveFloat(139.70000, 139.80000, 5)}',
-      // eslint-disable-next-line no-undef
-      price: getRandomPositiveInteger(10000, 1000000),
-      type: getRandomArrayElement(TYPES_OF_HOUSING),
-      rooms: getRandomPositiveInteger(1, 100),
-      guests: getRandomPositiveInteger(1, 100),
-      checkin: getRandomArrayElement(CHECKIN_TIMES),
-      checkout: getRandomArrayElement(CHECKOUT_TIMES),
-      features: createArr(ALL_FEATURES, 5),
-      description: 'Описание',
-      photos: createArr(ALL_PHOTOS, 3),
-    },
-    location: {
-      lat: getRandomPositiveFloat(35.65000,  35.70000, 5),
-      lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
-    },
-  });
+function getRandomPositiveFloat (min, max, digits = 1) {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
 
-  const similarAd = new Array(SIMILAR_COUNT).fill(null).map(() => createAd());
+  const result = Math.random() * (upper - lower) + lower;
 
-  similarAd;
+  return result.toFixed(digits);
+}
 
-  const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+function getRandomPositiveInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
 
-  // массив случайной длины из неповторяющихся значений. Из https://qna.habr.com/q/844269
-  const createArr = ([...source], maxLength) => Array.from(
-    { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
-    () => source.splice(Math.random() * source.length | 0, 1)[0]);
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-  createArr;
+  return Math.floor(result);
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const shuffleArr = ([...source], maxLength) => Array.from(
+  { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
+  () => source.splice(Math.random() * source.length | 0, 1)[0]);
+
+shuffleArr;
+
+const lat = getRandomPositiveFloat(35.65000,  35.70000, 5);
+const lon = getRandomPositiveFloat(139.70000, 139.80000, 5);
+
+const AVATARS = [];
+for ( let count = 1; count <= 11; count++ ) {
+  const zeroInt = (count < 10) ? 0 : '';
+  AVATARS.push(`img/avatars/user${  zeroInt  }${count  }.png`);
+}
+
+const createAd = () => ({
+  author: {
+    avatar: getRandomArrayElement(AVATARS),
+  },
+  offer: {
+    title: 'Заголовок',
+    address: `Широта: ${lat}, Долгота: ${lon}`,
+    price: getRandomPositiveInteger(10000, 1000000),
+    type: getRandomArrayElement(TYPES_OF_HOUSING),
+    rooms: getRandomPositiveInteger(1, 100),
+    guests: getRandomPositiveInteger(1, 100),
+    checkin: getRandomArrayElement(CHECKIN_TIMES),
+    checkout: getRandomArrayElement(CHECKOUT_TIMES),
+    features: shuffleArr(ALL_FEATURES, 5),
+    description: 'Описание',
+    photos: shuffleArr(ALL_PHOTOS, 3),
+  },
+  location: {
+    lat: lat,
+    lng: lon,
+  },
+});
+
+const similarAd = new Array(SIMILAR_COUNT)
+  .fill(null)
+  .map(createAd);
+
+similarAd;
