76484f79ce781ac0ffc1fee2564a4ecd753bb9fc
alsu <als@kxhfc.com>
2021-06-22 11:52:01 +0300

    Модуляция

---
+++ js/form/form.js
@@ -0,0 +1,3 @@
+import '../utils/utils.js';
+import '../server/server.js';
+import './validation/validation.js';
---
+++ js/map/map.js
@@ -0,0 +1,3 @@
+import '../utils/utils.js';
+import '../server/server.js';
+import './filter/filter.js';
