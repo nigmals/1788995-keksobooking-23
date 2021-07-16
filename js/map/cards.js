7fbb2e65ce561198e203209cf20dfe500fa1b0a1
alsu <als@kxhfc.com>
2021-07-16 13:46:56 +0300

    исправление ошибки вывода фото и удобств

--- js/map/cards.js
+++ js/map/cards.js
@@ -6,6 +6,9 @@ const TYPES = {
   hotel: 'Отель',
 };
 
+const PICTURE_WIDHT = 45;
+const PICTURE_HEIGHT = 40;
+
 const createCard = (similarOffer) => {
   const similarAdTemplate = document.querySelector('#card').content.querySelector('.popup');
   const offerElement = similarAdTemplate.cloneNode(true);
@@ -19,27 +22,33 @@ const createCard = (similarOffer) => {
   offerElement.querySelector('.popup__description').textContent = similarOffer.offer.description;
 
   const featuresList = offerElement.querySelector('.popup__features');
-  const fragment = document.createDocumentFragment();
   featuresList.innerHTML = '';
-  for (let index = 0; index < similarOffer.offer.features.length; index++) {
-    const featureNewElement = document.createElement('li');
-    featureNewElement.classList.add('popup__feature');
-    featureNewElement.classList.add(`popup__feature--${similarOffer.offer.features[index]}`);
-    fragment.appendChild(featureNewElement);
+  if (similarOffer.offer.features) {
+    similarOffer.offer.features.forEach((feature) => {
+      const item = document.createElement('li');
+      item.classList.add('popup__feature');
+      item.classList.add(`popup__feature--${feature}`);
+      featuresList.appendChild(item);
+    });
+  } else {
+    featuresList.classList.add('.visually-hidden');
   }
-  featuresList.appendChild(fragment);
 
-  offerElement.querySelector('.popup__description').textContent = similarOffer.offer.description;
-
-  const photosBlock = offerElement.querySelector('.popup__photos');
-  const photoElement = photosBlock.querySelector('.popup__photo');
-  photosBlock.removeChild(photoElement);
-  for (let index = 0; index < similarOffer.offer.photos.length; index++) {
-    const photoNewElement = photoElement.cloneNode(true);
-    photoNewElement.src = similarOffer.offer.photos[index];
-    fragment.appendChild(photoNewElement);
+  const photoAd = offerElement.querySelector('.popup__photos');
+  photoAd.innerHTML = '';
+  if (similarOffer.offer.photos) {
+    similarOffer.offer.photos.forEach((photo) => {
+      const picture = document.createElement('img');
+      picture.classList.add('popup__photo');
+      picture.src = photo;
+      picture.width = PICTURE_WIDHT;
+      picture.height = PICTURE_HEIGHT;
+      picture.alt = 'Фото жилья';
+      photoAd.appendChild(picture);
+    });
+  } else {
+    photoAd.classList.add('.visually-hidden');
   }
-  photosBlock.appendChild(fragment);
 
   if (similarOffer.offer.description === '') {
     offerElement.querySelector('.popup__description').classList.add('hidden');
